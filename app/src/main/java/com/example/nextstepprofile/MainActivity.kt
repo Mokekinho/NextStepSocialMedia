c1dd875d9e098400a36f67d70557c77cd8b38aa2
Moises <moisesprotazio@gmail.com>
2025-04-04 11:50:38 -0300

    Corection of last commit

--- app/src/main/java/com/example/nextstepprofile/MainActivity.kt
+++ app/src/main/java/com/example/nextstepprofile/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,8 +14,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -22,7 +28,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,19 +45,55 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
+            val profile = painterResource(R.drawable.perfiloficial)
+            val userName = "mokekinho"
+            val description = "My name is Moisés, I'm Brazilian and I'm trying to learn Kotlin and Android Development"
+            val feedImages = listOf(
+                painterResource(R.drawable.feed1),
+                painterResource(R.drawable.feed2),
+                painterResource(R.drawable.perfiloficial),
+                painterResource(R.drawable.feed1),
+                painterResource(R.drawable.feed2),
+                painterResource(R.drawable.perfiloficial),
+                painterResource(R.drawable.feed1),
+                painterResource(R.drawable.feed2),
+                painterResource(R.drawable.perfiloficial),
+                painterResource(R.drawable.feed1),
+                painterResource(R.drawable.feed2),
+                painterResource(R.drawable.perfiloficial),
+                painterResource(R.drawable.feed1),
+                painterResource(R.drawable.feed2),
+                painterResource(R.drawable.perfiloficial),
+                painterResource(R.drawable.feed1),
+                painterResource(R.drawable.feed2),
+                painterResource(R.drawable.perfiloficial),
+                painterResource(R.drawable.perfiloficial),
 
-            ProfileScreen()
+            )
+
+            ProfileScreen(
+                profile,
+                userName,
+                description,
+                feedImages,
+            )
+
 
         }
     }
 }
 
 @Composable
-fun ProfileScreen(){
+fun ProfileScreen(
+    profile : Painter,
+    userName : String,
+    description : String,
+    feedImages : List<Painter>
+){
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.DarkGray)
+            .background(Color.Black)
         ,
         horizontalAlignment = Alignment.CenterHorizontally,
 
@@ -56,18 +102,20 @@ fun ProfileScreen(){
             modifier = Modifier
                 .height(40.dp)
         )
-        Spacer( //it will be the photo
+        Image( //it will be the photo
+            profile,
+            null,
+            contentScale = ContentScale.Crop, //make it fill all the spaces
             modifier = Modifier
-                .background(Color.Green, shape = CircleShape)
-                .width(120.dp)
-                .height(120.dp)
+                .size(120.dp) //better because it get the height and the width
+                .clip(CircleShape) //cut the image in the circle shape
         )
         Spacer(
             modifier = Modifier
                 .height(30.dp)
         )
         Text(//username
-            "mokekinho",
+            userName,
             fontWeight = FontWeight.Bold,
             fontSize = 23.sp,
             color = Color.White,
@@ -77,7 +125,7 @@ fun ProfileScreen(){
                 .height(10.dp)
         )
         Text( //Descriprion
-            "My name is Moisés, I'm Brazilian and I'm trying to learn Kotlin and Android Development",
+            description,
             fontSize = 18.sp,
             color = Color.White,
             textAlign = TextAlign.Center,
@@ -102,49 +150,22 @@ fun ProfileScreen(){
                 .fillMaxWidth()
 
         )
-        LazyColumn(
-            modifier = Modifier
-                //LAZY COLUMN IS SCROLLABLE BY DEFAULT userScrollEnabled
-                .fillMaxSize()
+        LazyVerticalGrid(
+            GridCells.Fixed(3),
 
-            ,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            items(100){
-                Row (
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
+        ) {
+            items(feedImages){
+                Image(
+                    it,
+                    null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(1.dp)
+                        .align(Alignment.CenterHorizontally)
 
-                    Spacer( //images
-                        modifier = Modifier
-                            .width(150.dp)
-                            .height(150.dp)
-                            .padding(5.dp)
-                            .background(color = Color.White)
-                            .weight(1f)
-                    )
-                    Spacer( //images
-                        modifier = Modifier
-                            .width(150.dp)
-                            .height(150.dp)
-                            .padding(5.dp)
-                            .background(color = Color.White)
-                            .weight(1f)
-                    )
-                    Spacer( //images
-                        modifier = Modifier
-                            .width(150.dp)
-                            .height(150.dp)
-                            .padding(5.dp)
-                            .background(color = Color.White)
-                            .weight(1f)
-                    )
-
-
-                }
-
+                )
             }
-
         }
 
 
@@ -153,4 +174,42 @@ fun ProfileScreen(){
 
 }
 
+
+@Preview
+@Composable
+fun Preview(){
+    val profile = painterResource(R.drawable.perfiloficial)
+    val userName = "mokekinho"
+    val description = "My name is Moisés, I'm Brazilian and I'm trying to learn Kotlin and Android Development"
+    val feedImages = listOf(
+        painterResource(R.drawable.feed1),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.perfiloficial),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.feed1),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.perfiloficial),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.feed1),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.perfiloficial),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.feed1),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.perfiloficial),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.feed1),
+        painterResource(R.drawable.feed2),
+        painterResource(R.drawable.perfiloficial),
+        painterResource(R.drawable.feed2),
+
+    )
+
+    ProfileScreen(
+        profile,
+        userName,
+        description,
+        feedImages,
+    )
+}
 
