aa2047d2aab8cac6404f570f71deccdccbc8baf1
Moises <moisesprotazio@gmail.com>
2025-04-04 13:45:42 -0300

    Created the Button action

--- app/src/main/java/com/example/nextstepprofile/MainActivity.kt
+++ app/src/main/java/com/example/nextstepprofile/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,9 +24,17 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,11 +48,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.nextstepprofile.ui.theme.NextStepProfileTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
             val profile = painterResource(R.drawable.perfiloficial)
             val userName = "mokekinho"
@@ -51,25 +61,20 @@ class MainActivity : ComponentActivity() {
             val feedImages = listOf(
                 painterResource(R.drawable.feed1),
                 painterResource(R.drawable.feed2),
-                painterResource(R.drawable.perfiloficial),
+                painterResource(R.drawable.feed1),
+                painterResource(R.drawable.feed2),
                 painterResource(R.drawable.feed1),
                 painterResource(R.drawable.feed2),
                 painterResource(R.drawable.perfiloficial),
                 painterResource(R.drawable.feed1),
-                painterResource(R.drawable.feed2),
                 painterResource(R.drawable.perfiloficial),
                 painterResource(R.drawable.feed1),
-                painterResource(R.drawable.feed2),
                 painterResource(R.drawable.perfiloficial),
-                painterResource(R.drawable.feed1),
                 painterResource(R.drawable.feed2),
-                painterResource(R.drawable.perfiloficial),
-                painterResource(R.drawable.feed1),
-                painterResource(R.drawable.feed2),
-                painterResource(R.drawable.perfiloficial),
                 painterResource(R.drawable.perfiloficial),
 
             )
+
 
             ProfileScreen(
                 profile,
@@ -77,6 +82,7 @@ class MainActivity : ComponentActivity() {
                 description,
                 feedImages,
             )
+
 
 
         }
@@ -88,88 +94,120 @@ fun ProfileScreen(
     profile : Painter,
     userName : String,
     description : String,
-    feedImages : List<Painter>
+    feedImages : List<Painter>,
+    modifier: Modifier = Modifier
 ){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black)
-        ,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    val follows = remember{
+        mutableStateOf(true)
+    }
 
-    ) {
-        Spacer(
-            modifier = Modifier
-                .height(40.dp)
-        )
-        Image( //it will be the photo
-            profile,
-            null,
-            contentScale = ContentScale.Crop, //make it fill all the spaces
-            modifier = Modifier
-                .size(120.dp) //better because it get the height and the width
-                .clip(CircleShape) //cut the image in the circle shape
-        )
-        Spacer(
-            modifier = Modifier
-                .height(30.dp)
-        )
-        Text(//username
-            userName,
-            fontWeight = FontWeight.Bold,
-            fontSize = 23.sp,
-            color = Color.White,
-        )
-        Spacer(
-            modifier = Modifier
-                .height(10.dp)
-        )
-        Text( //Descriprion
-            description,
-            fontSize = 18.sp,
-            color = Color.White,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(start = 3.dp, end = 3.dp)
-        )
-        Spacer(
-            modifier = Modifier
-                .height(20.dp)
-        )
-        Button(
-            onClick = {
-                // implements the followAction
-            }
-        ) {
-            Text("Follow")
+
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
         }
-
-        Spacer(
+    ){
+        Column(
             modifier = Modifier
-                .height(20.dp)
-                .fillMaxWidth()
+                .fillMaxSize()
+                .background(Color.Black)
+                .padding(it)
+            ,
+            horizontalAlignment = Alignment.CenterHorizontally,
 
-        )
-        LazyVerticalGrid(
-            GridCells.Fixed(3),
+            ) {
+            Spacer(
+                modifier = Modifier
+                    .height(40.dp)
+            )
+            Image( //it will be the photo
+                profile,
+                null,
+                contentScale = ContentScale.Crop, //make it fill all the spaces
+                modifier = Modifier
+                    .size(120.dp) //better because it get the height and the width
+                    .clip(CircleShape) //cut the image in the circle shape
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(30.dp)
+            )
+            Text(
+//username
+                userName,
+                fontWeight = FontWeight.Bold,
+                fontSize = 23.sp,
+                color = Color.White,
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(10.dp)
+            )
+            Text( //Descriprion
+                description,
+                fontSize = 18.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(start = 3.dp, end = 3.dp)
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+            )
+            Button(
+                onClick = {
+                    if(follows.value) {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("You followed $userName")
+                        }
+                    }
+                    else {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("You unfollowed $userName")
+                        }
+                    }
+                    follows.value = !follows.value
 
-        ) {
-            items(feedImages){
-                Image(
-                    it,
-                    null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(150.dp)
-                        .padding(1.dp)
-                        .align(Alignment.CenterHorizontally)
+                    // implements the followAction
+                },
+                //colors = ButtonColors(Color.DarkGray,Color.White,Color.Gray,Color.White),
 
-                )
+            ) {
+                if (follows.value)
+                    Text("Follow")
+                else
+                    Text("Following")
             }
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+                    .fillMaxWidth()
+
+            )
+            LazyVerticalGrid(
+                GridCells.Fixed(3)
+
+            ) {
+                items(feedImages) {
+                    Image(
+                        it,
+                        null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(150.dp)
+                            .padding(1.dp)
+                            .align(Alignment.CenterHorizontally)
+
+                    )
+                }
+            }
+
+
         }
-
-
-
     }
 
 }
